f2f172661878cb8566567efc1367b62e88c8d18c
florian <florian.denzingergmx.ch>
2020-07-12 19:45:11 +0200

    hide streamlit

--- app.py
+++ app.py
@@ -89,3 +89,11 @@ scatter_chart = st.altair_chart(
         .interactive()
 )
 
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
