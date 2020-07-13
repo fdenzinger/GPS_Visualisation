ce3da41114db15a86ddf202d58ab9c0993b39ff7
florian <florian.denzingergmx.ch>
2020-07-13 23:12:51 +0200

    added download csv option and some improvements on plots; fixed tooltips

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pandas as pd
 import ssl
 import altair as alt
 import datetime
+import base64
 
 # get rid of ssl connection error (certificates)
 try:
@@ -13,8 +14,46 @@ except AttributeError:
 else:
     ssl._create_default_https_context = _create_unverified_https_context
 
+## password
+#from SessionState import get
+#session_state = get(password='')
+
+#if session_state.password != 'pwd123':
+#    pwd_placeholder = st.sidebar.empty()
+#    pwd = pwd_placeholder.text_input("Password:", value="", type="password")
+#    session_state.password = pwd
+#    if session_state.password == 'pwd123':
+#        pwd_placeholder.empty()
+#        main()
+#    elif session_state.password != '':
+#        st.error("the password you entered is incorrect")
+#else:
+#    main()
+
+def download_link(object_to_download, download_filename, download_link_text):
+    """
+    Generates a link to download the given object_to_download.
+
+    object_to_download (str, pd.DataFrame):  The object to be downloaded.
+    download_filename (str): filename and extension of file. e.g. mydata.csv, some_txt_output.txt
+    download_link_text (str): Text to display for download link.
+
+    Examples:
+    download_link(YOUR_DF, 'YOUR_DF.csv', 'Click here to download data!')
+    download_link(YOUR_STRING, 'YOUR_STRING.txt', 'Click here to download your text!')
+
+    """
+    if isinstance(object_to_download,pd.DataFrame):
+        object_to_download = object_to_download.to_csv(index=False)
+
+    # some strings <-> bytes conversions necessary here
+    b64 = base64.b64encode(object_to_download.encode()).decode()
+
+    return f'<a href="data:file/txt;base64,{b64}" download="{download_filename}">{download_link_text}</a>'
+
+
 st.sidebar.title('Site Selection')
-gpsSelector = st.sidebar.radio(
+gpsSelector = st.sidebar.selectbox(
     "Select site and GPS device",
     ("Grueebu: GPS 1",
      "Grueebu: GPS 3",
@@ -55,10 +94,13 @@ st.title('Geomon GPS Network')
 st.header(str(gpsSite) + ": " + gpsNo)
 st.subheader("Latest update: " + latestUpdate)
 
+if st.sidebar.button('Download source data as CSV'):
+    tmp_download_link = download_link(df, "Data_" + gpsSite + "_" + gpsNo + ".csv", 'Click here to download your data!')
+    st.sidebar.markdown(tmp_download_link, unsafe_allow_html=True)
+
 st.sidebar.subheader('Plot options')
 start_date = st.sidebar.date_input('Start date', min(df['Date']) - pd.Timedelta(days=5))
 end_date = st.sidebar.date_input('End date', max(df['Date'])+ pd.Timedelta(days=5))
-
 
 markerSize = st.sidebar.slider(
     'Set marker size',
@@ -71,9 +113,13 @@ scatter_chart = st.altair_chart(
         .mark_circle(size=markerSize, color = 'steelblue')
         .encode(alt.X('Date:T', scale=alt.Scale(domain=(start_date.strftime('%Y-%m-%d'), end_date.strftime('%Y-%m-%d')))),
                 alt.Y('Easting:Q', scale=alt.Scale(zero=False), title='Easting [m]'),
-                tooltip=['DateString', 'Easting'])
+                tooltip=[alt.Tooltip('DateString', title='Date'), 'Easting'])
+        .configure_axis(
+        labelFontSize=12,
+        titleFontSize=14
+        )
         .properties(
-            width=700,
+            width=800,
             height=250)
         .interactive()
 )
@@ -84,9 +130,13 @@ scatter_chart = st.altair_chart(
         .mark_circle(size=markerSize, color = 'seagreen')
         .encode(alt.X('Date:T', scale=alt.Scale(domain=(start_date.strftime('%Y-%m-%d'), end_date.strftime('%Y-%m-%d')))),
                 alt.Y('Northing:Q', scale=alt.Scale(zero=False), title='Northing [m]'),
-                tooltip=['DateString', 'Northing'])
+                tooltip=[alt.Tooltip('DateString', title='Date'), 'Northing'])
+        .configure_axis(
+        labelFontSize=12,
+        titleFontSize=14
+        )
         .properties(
-            width=700,
+            width=800,
             height=250)
         .interactive()
 )
@@ -97,12 +147,17 @@ scatter_chart = st.altair_chart(
         .mark_circle(size=markerSize, color = 'brown')
         .encode(alt.X('Date:T', scale=alt.Scale(domain=(start_date.strftime('%Y-%m-%d'), end_date.strftime('%Y-%m-%d')))),
                 alt.Y('Elevation:Q', scale=alt.Scale(zero=False), title='Elevation [m a.s.l.]'),
-                tooltip=['DateString', 'Elevation'])
+                tooltip=[alt.Tooltip('DateString', title='Date'), 'Elevation'])
+        .configure_axis(
+        labelFontSize=12,
+        titleFontSize=14
+        )
         .properties(
-            width=700,
+            width=800,
             height=250)
         .interactive()
 )
+
 
 hide_streamlit_style = """
             <style>
