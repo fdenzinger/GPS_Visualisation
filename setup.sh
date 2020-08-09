7abff071e6d58aeef359427580014167689a0937
bafu-DF <63240271+bafu-DF@users.noreply.github.com>
2020-08-09 13:00:46 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,5 +4,6 @@ echo "
 headless = true\n
 enableCORS=false\n
 port = $PORT\n
+token = "pk.eyJ1IjoiYmFmdS1kZiIsImEiOiJja2RteTIybGUxY3Z5MnhxMzJ6eTFkOXdqIn0.3lhLCtp3isEn6j1eObbLoQ"\n
 \n
 " > ~/.streamlit/config.toml
